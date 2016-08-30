afa1011e6816c44b7bdcc6d85af4f3240d63d241
Kelly Wright <kelly.wright@kellyinnovation.com>
2016-08-30 16:14:00 -0400

    User selects number of chances.

--- guess.py
+++ guess.py
@@ -3,7 +3,7 @@ import random
 magic_number = random.randrange(1,100,1)
 
 def game():
-	count = 10
+	count = int(input("How many guesses would you like?   "))
 	while (count > 0):
 		player_number = int(input("Choose a number between 1 and 100:  "))
 		if player_number == magic_number:
@@ -14,5 +14,6 @@ def game():
 		elif player_number > magic_number:
 			print("Your guess is too high.")
 		count = count - 1
+	print("You lost.")
 
 game()
