e9ebf0886c6f64fbfff5899b0e213ffcfac83df3
Kelly Wright <kelly.wright@kellyinnovation.com>
2016-09-29 09:28:53 -0400

    Update while.

--- guess_refactor.py
+++ guess_refactor.py
@@ -21,7 +21,7 @@ class Game():
 	def game(self):
 		print("Guess the magic number.")
 
-		while (self.guesses > 0):
+		while (self.guesses >= 0):
 			player_number = int(input("Choose a number between 1 and {}:  ".format(self.difficulty_choice)))
 			if player_number == self.magic_number:
 				print("You Win!")
@@ -30,8 +30,10 @@ class Game():
 				print("Your guess is too low.")
 			elif player_number > self.magic_number:
 				print("Your guess is too high.")
+
 			self.guesses -= 1
-		print("You lost. \nThe number was {}".format(self.magic_number))
+
+		print("You lost. \nThe number was {}".format(self.magic_number))			
 
 game_play = Game()
 game_play.get_magic_number()
